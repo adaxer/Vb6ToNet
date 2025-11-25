31458667e029cd5e7af7c9ee9d2a45c9574ccae6
Andreas Daxer <info@andreas-daxer.de>
2025-11-25 18:21:40 +0100

    AnlagenTemplates laden, In ComboBox anzeigen

--- BrexVB.Net/BrexVB.Net/B_Rex.Designer.vb
+++ BrexVB.Net/BrexVB.Net/B_Rex.Designer.vb
@@ -17,6 +17,9 @@ Partial Class B_Rex
     'Wird vom Windows Form-Designer benötigt.
     Private components As System.ComponentModel.IContainer
 
+    ' ComboBox-Feld für Templates (wird zur Laufzeit nach ReadData erstellt)
+    Private WithEvents cmbTemplates As System.Windows.Forms.ComboBox
+
     Public Sub New()
 
         ' Dieser Aufruf ist für den Designer erforderlich.
@@ -25,6 +28,25 @@ Partial Class B_Rex
         ' Fügen Sie Initialisierungen nach dem InitializeComponent()-Aufruf hinzu.
         StartCode.ReadData()
 
+        ' ComboBox erst nach dem Einlesen der Daten erstellen und füllen
+        cmbTemplates = New System.Windows.Forms.ComboBox()
+        With cmbTemplates
+            .Name = "cmbTemplates"
+            .DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+            .Dock = System.Windows.Forms.DockStyle.Top
+            .TabIndex = 0
+        End With
+
+        ' Direkter Zugriff auf StartCode.Templates (Index 1..12)
+        For i As Integer = 1 To Math.Min(12, StartCode.Templates.Length - 1)
+            Dim t = StartCode.Templates(i)
+            If t IsNot Nothing AndAlso Not String.IsNullOrEmpty(t.Bezeichnung) Then
+                cmbTemplates.Items.Add(t.Bezeichnung)
+            End If
+        Next
+
+        ' ComboBox zur Form hinzufügen
+        Me.Controls.Add(cmbTemplates)
     End Sub
 
     'Hinweis: Die folgende Prozedur ist für den Windows Form-Designer erforderlich.
@@ -34,7 +56,8 @@ Partial Class B_Rex
     Private Sub InitializeComponent()
         components = New System.ComponentModel.Container
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
+        ' Form-Größe verdoppelt
+        Me.ClientSize = New System.Drawing.Size(1600, 900)
         Me.Text = "B_Rex"
 
         ' Daten einlesen
@@ -43,4 +66,23 @@ Partial Class B_Rex
     Protected Overrides Sub Finalize()
         MyBase.Finalize()
     End Sub
+
+    ' Handler für Auswahlwechsel / "Click" eines Combobox-Items
+    Private Sub cmbTemplates_SelectedIndexChanged(sender As Object, e As EventArgs) Handles cmbTemplates.SelectedIndexChanged
+        Dim cb = DirectCast(sender, System.Windows.Forms.ComboBox)
+        If cb.SelectedIndex >= 0 Then
+            Dim selectedText = cb.SelectedItem?.ToString()
+            System.Diagnostics.Debug.WriteLine($"Template ausgewählt: {selectedText}")
+            ' Beispiel: Zugriff auf Template-Objekt, falls weitere Daten benötigt werden
+            Dim idx = cb.SelectedIndex + 1 ' Items starten bei 0, Templates bei 1
+            If idx >= 1 AndAlso idx <= 12 Then
+                Dim tmpl = StartCode.Templates(idx)
+                If tmpl IsNot Nothing Then
+                    ' TODO: Aktion mit tmpl.Anlage oder anderen Feldern
+                    System.Diagnostics.Debug.WriteLine($"Anlage-Inhalt Länge: {If(tmpl.Anlage, String.Empty).Length}")
+                End If
+            End If
+        End If
+    End Sub
+
 End Class
